2371d0fbd25677a1fcc4ee68e965872a99083d54
Lindsay Warr <iamalion@gmail.com>
2023-06-02 12:03:27 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 //Business logic
+window.onload = function () {
+    console.log("Script is executing!");
+
 
 
 //UI logic
-function addAnswers(event) {
+let form = document.getElementById("mainForm");
+form.onsubmit = function addAnswers(event) {
     event.preventDefault();
-
         const ans1 = parseInt(document.querySelector("input#readID").value);
         const ans2 = parseInt(document.querySelector("input#gameID").value);
         const ans3 = parseInt(document.querySelector("input#drawID").value);
@@ -14,9 +17,6 @@ function addAnswers(event) {
         const ans7 = parseInt(document.querySelector("input#gamesID").value);
         const ans8 = parseInt(document.querySelector("input#appsID").value);
         const ans9 = parseInt(document.querySelector("input#websitesID").value);
-        let js = 1;
-        let csharp = 2;
-        let css = 3;
         const sum = ans1+ans2+ans3+ans4+ans5+ans6+ans7+ans8+ans9;
         let result;
         if (sum <5 && sum >2) {
@@ -32,3 +32,4 @@ function addAnswers(event) {
         const form = document.getElementById("mainForm");
         form.addEventListener("submit", addAnswers)
     })
+}
