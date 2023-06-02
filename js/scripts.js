cf3dee24c391a6a52a4deb2ec42fa1361b3fb2f0
Lindsay Warr <iamalion@gmail.com>
2023-06-02 09:37:11 -0700

    Begin javascript

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Business logic
+let js = 0
+let csharp = 0
+let css = 0
+
+//UI logic
+function quiz(event) {
+    event.preventDefault();
+    const hobbies1 = parseInt(document.querySelector("input#readID").value);
+    const hobbies2 = parseInt(document.querySelector("input#gameID").value);
+    const hobbies3 = parseInt(document.querySelector("input#drawID").value);
+    const school1 = parseInt(document.querySelector("input#mathID").value);
+    const school2 = parseInt(document.querySelector("input#gymID").value);
+    const school3 = parseInt(document.querySelector("input#artID").value);
+    const skills1 = parseInt(document.querySelector("input#readID").value);
+    const skills2 = parseInt(document.querySelector("input#gameID").value);
+    const skills3 = parseInt(document.querySelector("input#drawID").value);
+}
