e9c232ad7ed9ae1c232da305fe03bc51c765b79d
Lindsay Warr <iamalion@gmail.com>
2023-06-02 13:56:48 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,8 @@ form.addEventListener("submit", function(event) {
             css: 3,
         }
     };
-    const form = event.target;
+    function addAnswers(event) {
+    event.preventDefault();
     const ans1 = parseInt(document.querySelector("input#readID").value);
     const ans2 = parseInt(document.querySelector("input#gameID").value);
     const ans3 = parseInt(document.querySelector("input#drawID").value);
@@ -35,17 +36,6 @@ form.addEventListener("submit", function(event) {
     const ans9 = parseInt(document.querySelector("input#websitesID").value);
     const sum = ans1+ans2+ans3+ans4+ans5+ans6+ans7+ans8+ans9;
     let result;
-    if (sum <5 && sum >2) {
-        result = "JavaScript";
-    } else if (sum <8 && sum >4) {
-        result = "C#";
-    } else if (sum <10 && sum >7) {
-        result = "CSS";
-    }
-    
-    });
-
-    let output = "";
     if (result < 5 && result > 2) {
     output = "JavaScript";
     } else if (result < 8 && result > 4) {
@@ -54,9 +44,9 @@ form.addEventListener("submit", function(event) {
     output = "CSS";
     }
     document.getElementById("output").textContent = "Your recommended language is: " + output;
-    }
+    };
     window.addEventListener("load", function () {
         const form = document.getElementById("mainForm");
         form.addEventListener("submit", addAnswers)
-    })
-}
+    });
+})
