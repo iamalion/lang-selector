fd66c00b0ff391f1b0f22be0e50e0cfb0f890a65
Lindsay Warr <iamalion@gmail.com>
2023-06-03 10:35:18 -0700

    Fix bugs

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,15 +1,18 @@
 function findAnswers (event) {
     event.preventDefault();
-    const ans1 = document.getElementById("readID").value;
-    const ans2 = document.getElementById("gameID").value;
-    const ans3 = document.getElementById("drawID").value;
-    const ans4 = document.getElementById("mathID").value;
-    const ans5 = document.getElementById("gymID").value;
-    const ans6 = document.getElementById("artID").value;
-    const ans7 = document.getElementById("gamesID").value;
-    const ans8 = document.getElementById("appsID").value;
-    const ans9 = document.getElementById("websitesID").value;
+    const hobby = document.getElementById("hobbyID").value;
+    const school = document.getElementById("schoolID").value;
+    const skills = document.getElementById("skillsID").value;
     let result;
+    if (hobby === "Reading" && school === "Math" && skills === "Make fun games"){
+        result = "JavaScript"
+    } else if (hobby === "Gaming" && school === "Gym" && skills === "Build useful mobile apps"){
+        result = "C#"
+    } else if (hobby === "Drawing" && school === "Art" && skills === "Create beautiful websites"){
+        result = "C/C++"
+    } else {
+        result = "Python"
+    }
     
     
     document.getElementById("output").innerText = "Your recommended language is " + result +"!!!"
